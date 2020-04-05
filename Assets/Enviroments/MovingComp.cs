249e9bb4d9ee3d2c4fd834e0fa84a2f8350441c0
rubyz <robyamama55@gmail.com>
2020-04-05 21:22:09 +0200

    Adding lights

---
+++ Assets/Enviroments/MovingComp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingComp : MonoBehaviour
+{
+    [SerializeField] Vector3 movementComponent; //mkan eele 3yz t7rko leh 
+    [Range(0, 1)] [SerializeField] float movementFactor;
+    [SerializeField] float period = 2f;
+    Vector3 startPostion;
+  
+
+
+    // Start is called once 
+    void Start()
+    {
+        startPostion = transform.position; //intial place , el transform postion mkano l asasy l awl
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period == 0) { return; }
+        float cycles = Time.time / period;  // number of periods
+        const float tau = Mathf.PI * 2f; //=2pi
+        float rawSineWave = Mathf.Sin(cycles * tau); //goes from -1 to +1 , it doesnot matter
+        movementFactor = rawSineWave / 2f + 0.5f; //to let movementFactor between 0 and +1
+       
+        Vector3 displacment = movementComponent * movementFactor; //el masafa ele hy5odha , displacment
+        transform.position = startPostion + displacment; // el transform.postion 3obara 3n el bdaya which is intial distance + final distance
+
+        
+    }
+}
---
+++ Assets/Enviroments/rocket.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rocket : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] AudioClip thrust;
+    [SerializeField] AudioClip sucess;
+    [SerializeField] AudioClip death;
+    [SerializeField] ParticleSystem thrustParticles;
+    [SerializeField] ParticleSystem sucessParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] float LoadLevelDelay = 1f;
+   
+
+    new Rigidbody rigidbody;
+    AudioSource audioSource;
+    enum State { Alive, Dying, Loading };
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>(); // El music ele 7attha dakhlt gwa l audiosource 3lshan anaa 3arfto 3la el AudioSource
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive) // da 3lshan lma ymot myyfdlsh mkml ene a2dr al3bb beh , f bystna lhd ma l state trg3 tany tt8yr lel intial
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+
+
+
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive)
+        {
+            return;
+        }
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartSucessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+
+
+        }
+        void StartSucessSequence()
+        {
+
+            state = State.Loading;
+            audioSource.Stop();
+            audioSource.PlayOneShot(sucess);
+            sucessParticles.Play(); // for particles
+            Invoke("LoadNextLevel", LoadLevelDelay);
+
+
+
+        }
+        void StartDeathSequence()
+
+
+        {
+            state = State.Dying;
+            audioSource.Stop();
+            audioSource.PlayOneShot(death);
+            thrustParticles.Stop(); // i added it
+            deathParticles.Play();
+            Invoke("LoadFirstLevel", LoadLevelDelay); // parameterise time
+        }
+
+    }
+
+   void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void RespondToThrustInput()
+
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+
+        }
+        else
+        {
+            audioSource.Stop();
+            thrustParticles.Stop();
+        }
+
+
+    }
+    void ApplyThrust()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * mainThrust*Time.deltaTime); //frame rate inpendent
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(thrust);
+
+            Debug.Log("lol");
+            thrustParticles.Play();
+        }
+       
+
+
+
+
+
+    }
+    void RespondToRotateInput()
+    {
+       
+      
+        ApplyRotate();
+    }
+
+
+        void ApplyRotate()
+        {
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+        rigidbody.freezeRotation = true; // let physics while prerssing the keys be off 
+        if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward * rotationSpeed);
+            }
+
+
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(-Vector3.forward * rotationSpeed);
+            }
+        rigidbody.freezeRotation = false; //;
+    }
+   
+    
+}
